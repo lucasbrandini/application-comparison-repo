f07e9ade53348b87a8f5426b8659ce1700bb843f
VictorOkayama <vokayama.itchiro@hotmail.com>
2024-03-05 15:45:44 -0300

    Melhoria <> Implementacao de botao search

--- react/src/components/Search.jsx
+++ react/src/components/Search.jsx
@@ -1,10 +1,19 @@
 import style from './Search.module.css'
+import { useState } from 'react'
 
 export function Search() {
+  const [searchTxt, setTxt] = useState ('')
+
+  const clickEvent = () => {
+    setTxt(SearchInput.value)
+  }
+
   return (
     <>
       <div>
-        <input placeholder="Digite o texto desejado aqui" type='text' className={style.input} />
+        <input id='SearchInput' placeholder="Digite o texto desejado aqui" type='text' className={style.input} />
+        <button onClick = {clickEvent} >🔎</button>
+        <h2>{searchTxt}</h2>
       </div>
     </>
   )

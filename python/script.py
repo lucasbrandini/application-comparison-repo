b6c6341b394259e94e10c28a8138397030bb4e46
lucascriado <lucaasbrandini@gmail.com>
2024-03-15 19:40:33 -0300

    feat: improve python

--- python/script.py
+++ python/script.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/main', methods=['GET'])
+data = []
+
+@app.route('/main', methods=['GET', 'POST'])
 def main():
-    return 'Welcome to the Flask App!'
+    if request.method == 'POST':
+        name = request.json.get('name')  
+        content = request.json.get('content')  
+        print(f'Name: {name}, Content: {content}')  
+        data.append({'name': name, 'content': content})
+        return redirect(url_for('show_data'))
+
+    return jsonify(data)
+
+@app.route('/show_data')
+def show_data():
+    return render_template('data.html', data=data)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)

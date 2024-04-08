e896631bba59f8ffb4392fffb5e48792aff897fe
RenamRR <64269144+RenamRR@users.noreply.github.com>
2024-04-07 22:53:49 -0300

    Up <> Teste Vitão

--- nodejs/script.js
+++ nodejs/script.js
@@ -17,7 +17,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);

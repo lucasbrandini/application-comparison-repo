88abfbf75a0143fdec84b886d0f82e452ce9765e
lucascriado <lucaasbrandini@gmail.com>
2024-04-09 20:06:41 -0300

    feat: python e html

--- python/script.py
+++ python/script.py
@@ -1,16 +1,8 @@
 import requests
 import os
-from sqlalchemy import create_engine, Table, MetaData
-from sqlalchemy.orm import sessionmaker
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from pybars import Compiler
-
-engine = create_engine('mysql+pymysql://admin:Password1!@localhost/python')
-metadata = MetaData()
-table = Table('table_name', metadata, autoload_with=engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+import mysql.connector
 
 compiler = Compiler()
 
@@ -20,19 +12,6 @@ def render_template(template_name, data):
         source = file.read()
     template = compiler.compile(source)
     output = template(data)
-    return output
-
-def handle_home():
-    results = session.query(table).all()
-    data = [row._asdict() for row in results]
-    print(data, 'data')
-    output = render_template('home.hbs', data)
-    return output
-
-def handle_data():
-    results = session.query(table).all()
-    data = [row._asdict() for row in results]
-    output = render_template('data.hbs', data)
     return output
 
 def handle_about():
@@ -50,10 +29,8 @@ def handle_404():
     return "Rota nao encontrada."
 
 routes = {
-    '/home': handle_home,
     '/about': handle_about,
     '/posts': handle_posts,
-    '/data': handle_data,
 }
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
